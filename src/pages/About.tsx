956a4beac01f3d28bcc90a60626f42fc6240494f
Gediminas Survila <survila.gediminas@gmail.com>
2025-06-09 12:34:30 +0300

    new string added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,12 @@ import { useTranslation } from "react-i18next";
 
 const About: React.FC = () => {
   const { t } = useTranslation();
-  return <h1>{t("about_us")}</h1>;
+  return (
+    <div>
+      <h1>{t("about_us")}</h1>
+      <div>{t("about_description")}</div>
+    </div>
+  );
 };
 
 export default About;
